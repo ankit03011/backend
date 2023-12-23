71d6af9daf97a5395d027305b4dcfb14edd5a1e3
ankit03011 <ankitmech2016@gmail.com>
2023-12-23 20:01:10 +0530

    third commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
 const app = express()
-const port = 5000
+const port = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
